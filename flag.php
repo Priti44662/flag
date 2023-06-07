5c61d6ebac586c116167a0ce2ebf4c6c21ddec6a
Priti44662 <135731060+Priti44662@users.noreply.github.com>
2023-06-07 09:58:05 +0530

    Add files via upload

---
+++ flag.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1" height="300px" width="600px">
+        <tr>
+            <td colspan="3" height="100px" width="300px" bgcolor="orange"></td>
+        </tr>
+        <tr>
+            <td colspan="3" height="100px" width="300px" ><center><img src="" alt="noimge" height="110px" width="110px"></center></td>
+        </tr>
+        <tr>
+            <td colspan="3" height="100px" width="300px" bgcolor="green"></td>
+        </tr>
+    </table>
+</body>
+</html>
